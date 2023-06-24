5e5555466d1ad74cb2db1b6a27ead90abb788a66
Ricardo Chirinos Rosas <ricardo.chirinosr@usil.pe>
2023-06-24 01:44:14 -0500

    PaginaController

---
+++ src/main/java/com/trabfinal/dojo/controllers/PaginaController.java
@@ -0,0 +1,12 @@
+package com.trabfinal.dojo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PaginaController {
+    @GetMapping("/preguntas")
+    public String preguntas(){
+        return "frames/preguntas";
+    }
+}
